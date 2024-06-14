5e547302337b7091149b8454589438076ae6bb3c
SJHlll <ssaadd1212@gmail.com>
2024-06-14 13:22:55 +0900

    중간저장

--- src/KakaoMap/KakaoMap.js
+++ src/KakaoMap/KakaoMap.js
@@ -1,32 +1,10 @@
-import { Map, ZoomControl } from 'react-kakao-maps-sdk';
-import KakaoMapMarker from './KakaoMapMarker';
 import { useState } from 'react';
+import { Map, ZoomControl, useKakaoLoader } from 'react-kakao-maps-sdk';
+import KakaoMapMarker from './KakaoMapMarker';
 
 const KakaoMap = () => {
-  // 마커 클릭 시 창 열림
-  const openWindow = (index) => {
-    const updatedMarkers = markers.map((marker, i) =>
-      i === index ? { ...marker, isOpen: true } : { ...marker, isOpen: false },
-    );
-    setMarkers(updatedMarkers);
-  };
-
-  // 창닫기 클릭시 창 닫힘
-  const closeWindow = (index) => {
-    const updatedMarkers = markers.map((marker, i) =>
-      i === index ? { ...marker, isOpen: false } : marker,
-    );
-    setMarkers(updatedMarkers);
-  };
-
-  const handleMapClick = () => {
-    // 맵의 배경 클릭 시 창 닫힘
-    const updatedMarkers = markers.map((marker) => ({
-      ...marker,
-      isOpen: false,
-    }));
-    setMarkers(updatedMarkers);
-  };
+  useKakaoLoader();
+  const [center, setCenter] = useState({ lat: 37.552484, lng: 126.937641 }); // 지도 초기 위경도
 
   const [markers, setMarkers] = useState([
     {
@@ -47,14 +25,41 @@ const KakaoMap = () => {
     },
   ]);
 
+  // 마커 클릭 시 창 열림
+  const openWindow = (index) => {
+    const clickedMarker = markers[index];
+    setMarkers(
+      markers.map((marker, i) => ({
+        ...marker,
+        isOpen: i === index,
+      })),
+    );
+    setCenter({ lat: clickedMarker.lat, lng: clickedMarker.lng });
+  };
+
+  // 창닫기 클릭시 창 닫힘
+  const closeWindow = (index) => {
+    const updatedMarkers = markers.map((marker, i) =>
+      i === index ? { ...marker, isOpen: false } : marker,
+    );
+    setMarkers(updatedMarkers);
+  };
+
+  const handleMapClick = () => {
+    // 맵의 배경 클릭 시 창 닫힘
+    const updatedMarkers = markers.map((marker) => ({
+      ...marker,
+      isOpen: false,
+    }));
+    setMarkers(updatedMarkers);
+  };
+
   return (
     <>
       <Map
         id='map'
-        center={{
-          lat: 37.552484, // 맨 처음 보여줄 위도
-          lng: 126.937641, // 맨 처음 보여줄 경도
-        }}
+        center={center}
+        isPanto={true}
         style={{
           width: '1280px',
           height: '798px',
