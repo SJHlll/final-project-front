9e82d3b368a626a2a59d9d8a894f9d2c34b36768
Dasom04 <slz_952@naver.com>
2024-06-17 20:35:35 +0900

    카카오 로그인 구현

---
+++ src/components/user/KakaoLoginHandler.js
@@ -0,0 +1,45 @@
+import React, { useContext, useEffect } from 'react';
+import { API_BASE_URL, USER } from '../../config/host-config';
+import AuthContext from '../../util/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+const KakaoLoginHandler = () => {
+  console.log(
+    '사용자가 동의화면을 통해 필수 정보 동의 후 kakao인증 서버에서 redirect를 진행함!',
+  );
+
+  const { onLogin } = useContext(AuthContext);
+  const redirection = useNavigate();
+
+  const REQUEST_URL = API_BASE_URL + USER;
+
+  // URL에 쿼리스트링으로 전달된 인가 코드를 얻어오는 방법.
+  const code = new URL(window.location.href).searchParams.get('code');
+
+  console.log('REQUEST_URL:', REQUEST_URL);
+  console.log('Code:', code);
+
+  useEffect(() => {
+    // 컴포넌트가 렌더링될때 인가 코드를 백엔드로 전송하는 fetch 요청
+    const kakaoLogin = async () => {
+      try {
+        const res = await fetch(REQUEST_URL + '/kakaoLogin?code=' + code);
+
+        const { token, userName, email, role } = await res.json(); // 서버에서 온 json 읽기
+
+        // Context API를 사용하여 로그인 상태를 업데이트 합니다.
+        onLogin(token, userName, role);
+
+        redirection('/');
+      } catch (error) {
+        console.error('Error during Kakao login:', error);
+      }
+    };
+
+    kakaoLogin();
+  }, [code, onLogin, redirection, REQUEST_URL]);
+
+  return <div>KakaoLoginHandler</div>;
+};
+
+export default KakaoLoginHandler;
---
+++ src/config/naver-config.js
@@ -0,0 +1,5 @@
+const CLIENT_ID = process.env.REACT_APP_REST_API_NAVER_KEY;
+const REDIRECT_URI = process.env.REACT_APP_REDIRECT_NAVER_URL;
+const STATE = Math.random().toString(36).substring(2);
+
+export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&state=${STATE}&prompt=login`;
