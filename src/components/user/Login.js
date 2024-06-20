53de1efe645683560145eb0bfeb83e1837aa851f
seungjae0908 <tmtmdd123@naver.com>
2024-06-20 17:43:51 +0900

    git reset hard option to resolved file upload issue

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,11 +1,18 @@
 import { Grid, TextField } from '@mui/material';
 import React from 'react';
-import { Container } from 'reactstrap';
+import { Button, Container } from 'reactstrap';
 // import '../../scss/Login.scss';
 import { KAKAO_AUTH_URL } from '../../config/kakao-config';
 import { NAVER_AUTH_URL } from '../../config/naver-config';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const click = (text) => {
+    // 특정 경로로 네비게이션
+    navigate(text);
+  };
   return (
     <>
       <Container className='body-top'>
@@ -37,6 +44,12 @@ const Login = () => {
             // src={require('../../assets/kakao_login_medium_wide.png')}
           />
         </Grid>
+        <Button
+          className='testbtn'
+          onClick={() => click('/choose')}
+        >
+          우선 넘기기
+        </Button>
       </Container>
     </>
   );
