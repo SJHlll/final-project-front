2757aaeab307981da7b8410bd9c159dd5b5e9243
seungjae0908 <tmtmdd123@naver.com>
2024-06-21 19:00:55 +0900

    app.js수정

--- src/App.js
+++ src/App.js
@@ -22,55 +22,55 @@ const App = () => {
   return (
     <ModalProvider>
       <AuthContextProvider>
-    <div className='App'>
-      <Testheader />
-      <Routes>
-        {/* 메인 홈페이지 */}
-        <Route exact path='/' element={<Testhome />} />
-        {/* 이용방법 페이지 */}
-        <Route path='/noti' element={<Noti />} />
-        {/* 이벤트 페이지 */}
-        <Route path='/event' element={<Event />} />
-        {/* 이용후기 페이지 */}
-        <Route path='/review' element={<Review />} />
-        {/* 충전소 메인 페이지 */}
-        <Route
-          path='/charge/home'
-          element={<ChargeMain />}
-        />
-        <Route path='/charge/test' element={<Test />} />
-        {/* 충전소 페이지 */}
-        <Route
-          path='charge/list'
-          element={<ChargeStation />}
-        />
-        <Route
-          path='charge/reservation'
-          element={<ReservationCharge />}
-        />
+        <div className='App'>
+          <Testheader />
+          <Routes>
+            {/* 메인 홈페이지 */}
+            <Route exact path='/' element={<Testhome />} />
+            {/* 이용방법 페이지 */}
+            <Route path='/noti' element={<Noti />} />
+            {/* 이벤트 페이지 */}
+            <Route path='/event' element={<Event />} />
+            {/* 이용후기 페이지 */}
+            <Route path='/review' element={<Review />} />
+            {/* 충전소 메인 페이지 */}
+            <Route
+              path='/charge/home'
+              element={<ChargeMain />}
+            />
+            <Route path='/charge/test' element={<Test />} />
+            {/* 충전소 페이지 */}
+            <Route
+              path='charge/list'
+              element={<ChargeStation />}
+            />
+            <Route
+              path='charge/reservation'
+              element={<ReservationCharge />}
+            />
 
-        {/* 마이페이지 */}
-        <Route path='/mypage' element={<Mypage />} />
+            {/* 마이페이지 */}
+            <Route path='/mypage' element={<Mypage />} />
 
-        {/* 공통 로그인페이지 */}
-        <Route path='/Login' element={<Login />} />
-        {/* 카카오 로그인페이지 */}
-        <Route
-          path='/oauth/kakao'
-          element={<LoginSuccess />} // 강아지 사진 나오면 로그인 성공
-        />
-        {/* 네이버 로그인페이지 */}
-        <Route
-          path='/oauth/naver'
-          element={<LoginSuccess />} // 강아지 사진 나오면 로그인 성공
-        />
+            {/* 공통 로그인페이지 */}
+            <Route path='/Login' element={<Login />} />
+            {/* 카카오 로그인페이지 */}
+            <Route
+              path='/oauth/kakao'
+              element={<LoginSuccess />} // 강아지 사진 나오면 로그인 성공
+            />
+            {/* 네이버 로그인페이지 */}
+            <Route
+              path='/oauth/naver'
+              element={<LoginSuccess />} // 강아지 사진 나오면 로그인 성공
+            />
 
-        {/* 구글 로그인페이지 */}
+            {/* 구글 로그인페이지 */}
 
-        {/* 에러페이지 */}
-        <Route path='/*' element={<Error />} />
-      </Routes>
-    </div>
+            {/* 에러페이지 */}
+            <Route path='/*' element={<Error />} />
+          </Routes>
+        </div>
       </AuthContextProvider>
     </ModalProvider>
   );
--- src/Car/components/Mainpage/Event.js
+++ src/Car/components/Mainpage/Event.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Tabline from './Tabline';
 import './Event.scss';
-import car from '../../assets/car2.png';
 const Event = () => {
   return (
     <div className='maincontainer'>
