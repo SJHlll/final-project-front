a10e1d9c5773015263e0a647024742b2220ce14f
SJHlll <ssaadd1212@gmail.com>
2024-06-25 12:02:20 +0900

    급속, 완속, 시간에 따라 가격, 충전량 표시

--- src/charge/components/reservation_charge/ReservationModal.js
+++ src/charge/components/reservation_charge/ReservationModal.js
@@ -1,9 +1,9 @@
 import { addDays, setHours, setMinutes } from 'date-fns';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.module.css';
 import '../scss/ReservationModal.scss';
-import SpeedSelect from './SpeedSelect';
+// import SpeedSelect from './SpeedSelect';
 import { Button } from 'reactstrap';
 import OpenTossPayments from '../../../components/pay/OpenTossPayments';
 
@@ -18,6 +18,8 @@ const ReservationModal = ({
     setHours(setMinutes(new Date(), 0), 9), // 오늘 날짜에 9시 0분으로
   );
 
+  const [selectedValue, setSelectedValue] = useState(10);
+
   const filterPassedTime = (time) => {
     const currentDate = new Date();
     const selectedDate = new Date(time);
@@ -28,6 +30,68 @@ const ReservationModal = ({
   // submit 이벤트 핸들러
   const reservationHandler = (e) => {
     e.preventDefault();
+  };
+
+  // 시간, 분 정하기
+  const [selectedOption, setSelectedOption] =
+    useState('option1');
+
+  const handlerRadioChange = (e) => {
+    setSelectedOption(e.target.value);
+    if (e.target.value === 'option1') {
+      setSelectedValue(10); // 기본값 설정
+    } else if (e.target.value === 'option2') {
+      setSelectedValue(1); // 기본값 설정
+    }
+  };
+
+  useEffect(() => {
+    if (speed === '급속') {
+      setSelectedOption('option1');
+    } else if (speed === '완속') {
+      setSelectedOption('option2');
+    }
+  }, [speed]);
+
+  const FAST = [
+    { value: '10', name: '10분' },
+    { value: '20', name: '20분' },
+    { value: '30', name: '30분' },
+    { value: '40', name: '40분' },
+    { value: '50', name: '50분' },
+    { value: '60', name: '60분' },
+  ];
+
+  const SLOW = [
+    { value: '10', name: '1시간' },
+    { value: '20', name: '2시간' },
+    { value: '30', name: '3시간' },
+    { value: '40', name: '4시간' },
+    { value: '50', name: '5시간' },
+    { value: '60', name: '6시간' },
+    { value: '70', name: '7시간' },
+    { value: '80', name: '8시간' },
+  ];
+
+  const SelectBox = (props) => {
+    return (
+      <select
+        value={selectedValue}
+        onChange={(e) =>
+          setSelectedValue(Number(e.target.value))
+        }
+      >
+        {props.options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+    );
+  };
+
+  const calculateTotalPrice = () => {
+    return price * selectedValue;
   };
 
   return (
@@ -61,7 +125,46 @@ const ReservationModal = ({
               dateFormat={'yyyy년 MM월 dd일 aa hh:mm'}
             />
           </div>
-          <SpeedSelect speed={speed} />
+          <div className='flex'>
+            <div className='column'>충전량</div>
+            <div className='form-check form-check-inline'>
+              <input
+                className='form-check-input'
+                type='radio'
+                name='inlineRadioOptions'
+                id='inlineRadio1'
+                value='option1'
+                checked={selectedOption === 'option1'}
+                onChange={handlerRadioChange}
+                disabled
+              />
+              <label className='check-label'>급속</label>
+            </div>
+            <div className='form-check form-check-inline'>
+              <input
+                className='form-check-input'
+                type='radio'
+                name='inlineRadioOptions'
+                id='inlineRadio2'
+                value='option2'
+                checked={selectedOption === 'option2'}
+                onChange={handlerRadioChange}
+                disabled
+              />
+              <label className='check-label'>완속</label>
+            </div>
+          </div>
+
+          <div className='flex'>
+            <div className='column'>충전시간</div>
+            {selectedOption === 'option1' && (
+              <SelectBox options={FAST} defaultValue='10' />
+            )}
+            {selectedOption === 'option2' && (
+              <SelectBox options={SLOW} defaultValue='1' />
+            )}
+          </div>
+          {/* 미친짓 끝 */}
           <div className='flex'>
             <div className='column'>충전소 이름</div>
             <div className='data'>{name}</div>
@@ -76,14 +179,22 @@ const ReservationModal = ({
           </div>
           <div className='flex'>
             <div className='column'>가격</div>
-            <div className='data'>{price}원 (##kWh)</div>
+            <div className='data'>
+              {calculateTotalPrice()}원 (약{' '}
+              {speed === '급속'
+                ? selectedValue * 1.7
+                : selectedValue}
+              kWh)
+            </div>
             <Button
               variant='outlined'
               color='success'
               size='small'
               style={{ marginLeft: 'auto' }}
             >
-              <OpenTossPayments totalPrice={price} />
+              <OpenTossPayments
+                totalPrice={calculateTotalPrice()}
+              />
             </Button>
           </div>
         </form>
