bd3a83420ceaa8b3cab2d847090d32ff6e63e672
seungjae0908 <tmtmdd123@naver.com>
2024-06-26 14:14:29 +0900

    전반적인 css 수정

---
+++ src/Car/components/Mainpage/carreservation/Carreservation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Carres from '../../CarHeader/Carres';
+
+const Carreservation = () => {
+  return (
+    <div className='maincontainer'>
+      <div className='contentline'>
+        <div className='carreservationbody'>
+          <Carres />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Carreservation;
